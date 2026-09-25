66ae50bcc23a992ed18bae08ec2d4a490e15c9a9
Kim yohan <kimyohan@Kimui-MacBookAir.local>
2026-09-25 12:19:40 +0900

    Fix PWA cache update logic in Service Worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hangul-v2';
+const CACHE_NAME = 'hangul-v3';
 const urlsToCache = [
   './',
   './index.html',
@@ -11,9 +11,24 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
